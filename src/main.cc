cbd56664b134fd31f7b92bca6896de2593981358
VC Science Labs <ahmed_rizvi1@my.vcccd.edu>
2018-09-18 08:44:06 -0700

    Turning in

---
+++ src/main.cc
@@ -0,0 +1,19 @@
+/**
+* CS V13 Object-Oriented Programming
+* CRN: [CHANGE THIS INFORMATION]
+* Assignment: EX01-HelloWorld
+*
+* Statement of code ownership: I hereby state that I have written all of this
+* code and I have not copied this code from any other person or source.
+*
+* @author [ahmed_rizvi1@my.vcccd.edu]
+*/
+
+#include <iostream>
+#include <csv13>
+using namespace std;
+int main() {
+
+	cout << "Hello, World!" << endl;
+	return 0;
+}
